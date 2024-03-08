fbac8f7b3e4e1070561e475d221d0c63f383f439
Barbara Ribeiro <barbararibeiro95@outlook.com>
2024-03-08 16:03:36 -0300

    feat: first requirement: 4/6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Provider store={ store }>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+      </Routes>
+    </Provider>  
+  )
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { updateEmail } from '../redux/actions/index';
+import { UPDATE_EMAIL } from '../redux/actions/index';
+
+
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [btnEnable, setBtnEnable] = useState(false);
+
+  const loginRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+  
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+  
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(updateEmail(email));
+    navigate('/carteira');
+  };
+  
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  
+  useEffect(() => { 
+    console.log('Email: ', email);
+    console.log('Password: ', password);
+    const isEmailValid = loginRegex.test(email);
+    const isPasswordValid = password.length >= 6;
+    console.log('Is email valid: ', isEmailValid);
+    console.log('Is password valid: ', isPasswordValid);
+    setBtnEnable(isEmailValid && isPasswordValid);
+    console.log('Button enabled: ', btnEnable);
+  }, [email, password]); 
+
+  return (
+    <div>
+      <form action="">
+      <input 
+        type="email" 
+        name="email"
+        data-testid='email-input' 
+        value={email} 
+        onChange={handleEmailChange}
+      />
+        <input 
+          type="password" 
+          name="password" 
+          data-testid='password-input' 
+          value={password} 
+          onChange={handlePasswordChange}
+          autoComplete="current-password"
+        />     
+        <button 
+          disabled={!btnEnable}
+          onClick={(e) => handleLogin(e)}
+        >
+        Entrar
+        </button>
+      </form>      
+    </div>
+  )
 }
 
 export default Login;
